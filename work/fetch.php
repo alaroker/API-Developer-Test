4a7a26ed5422784f13bb3e616034f963c07e9a90
alaroker <julietalaroker@gmail.com>
2020-07-16 23:01:05 +0000

    display table data

--- work/fetch.php
+++ work/fetch.php
@@ -14,14 +14,30 @@ $result = json_decode($response);
 
 $output = '';
 
+// display table data onto the web page
+
 if(count($result) > 0)
 {
  foreach($result as $row)
  {
   $output .= '
   <tr>
-   <td>'.$row->first_name.'</td>
-   <td>'.$row->last_name.'</td>
+   <td>'.$row->county.'</td>
+   <td>'.$row->country.'</td>
+   <td>'.$row->town.'</td>
+   <td>'.$row->description.'</td>
+   <td>'.$row->details.'</td>
+   <td>'.$row->address.'</td>
+   <td>'.$row->image.'</td>
+   <td>'.$row->thumbnail.'</td>
+   <td>'.$row->latitude.'</td>
+   <td>'.$row->longitude.'</td>
+   <td>'.$row->bedrooms.'</td>
+   <td>'.$row->bathrooms.'</td>
+   <td>'.$row->price.'</td>
+   <td>'.$row->type.'</td>
+   <td>'.$row->saleorrent.'</td>
+   
    <td><button type="button" name="edit" class="btn btn-warning btn-xs edit" id="'.$row->id.'">Edit</button></td>
    <td><button type="button" name="delete" class="btn btn-danger btn-xs delete" id="'.$row->id.'">Delete</button></td>
   </tr>
